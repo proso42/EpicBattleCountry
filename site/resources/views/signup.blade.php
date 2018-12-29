2716e0378dacd83e779cf974ccaf242c4be6dfcf
roso claude <rosoclaude@imac-de-roso.home>
2018-12-29 18:35:31 +0100

    add csrf

--- site/resources/views/signup.blade.php
+++ site/resources/views/signup.blade.php
@@ -38,6 +38,7 @@
                 </br>
                 <input id="sponsor" class="signup-input" placeholder="Parrain" type="text">
                 </br>
+                {{csrf_filed()}}
                 <button class="signup-button" type="submit">S'inscrire</button>        
             </form>
             <hr class="signup-footer"/>

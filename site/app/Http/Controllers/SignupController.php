8289563bc5c13cec854628a01e4f3ce1a218b8bb
Philippe <proso@student.42.fr>
2019-01-28 15:16:00 +0100

    email confirm 1.0.1
    dd $request to debug

--- site/app/Http/Controllers/SignupController.php
+++ site/app/Http/Controllers/SignupController.php
@@ -39,6 +39,7 @@
 
         public function register(Request $request)
         {
+            dd($request);
             if ($request['password'] !== $request['password2'])
                 return view('signup_internal_fail');
             $id_race = DB::table('races')

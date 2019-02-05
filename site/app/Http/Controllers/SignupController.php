2477d6841c1a543953436319d37bc22d0fbfb41c
proso <proso@student.42.fr>
2019-02-05 16:59:00 +0100

    email confirm 1.2.6
    fix bad name of var ;

--- site/app/Http/Controllers/SignupController.php
+++ site/app/Http/Controllers/SignupController.php
@@ -57,9 +57,9 @@
             ->insert(
                 array('user_id' => $user_id, 'user_email' => $email, 'link' => $link, 'status' => 'Waiting')
             );
-            $outpout = array();
+            $output = array();
             $cmd = 'cd ~/www/scripts ; node send_mail.js ' . $login . ' ' . $email  . ' \"' . $link . '\"';
-            exec($cmd, $outpout);
+            exec($cmd, $output);
             dd($output);
             return view('register_success', compact('email', 'cmd', 'ret'));
         }

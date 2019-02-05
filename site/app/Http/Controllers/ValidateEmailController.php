d39408531a513e8058f65eb3f28613f6d38d2ec5
Unknown <proso@student.42.fr>
2019-02-05 23:18:16 +0100

    confirm email 1.4.8
    with compact is better ...

--- site/app/Http/Controllers/ValidateEmailController.php
+++ site/app/Http/Controllers/ValidateEmailController.php
@@ -38,7 +38,7 @@
                 DB::table('users')
                 ->where('email', '=', $user_email)
                 ->update(['email_verified_at' => time()]);
-                return view('validate_email_success', 'user_email');
+                return view('validate_email_success', compact('user_email'));
             }
             else
                 return view('validate_email_failed');

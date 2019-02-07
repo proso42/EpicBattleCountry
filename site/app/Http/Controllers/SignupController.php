5ca1cedb219b9e2f741a9371c046ecdbaef79496
proso <proso@student.42.fr>
2019-02-07 20:27:28 +0100

    login 1.1.9
    fix missing '_' in password_hash ;

--- site/app/Http/Controllers/SignupController.php
+++ site/app/Http/Controllers/SignupController.php
@@ -45,7 +45,7 @@
             ->select('id')
             ->where('race_name', '=', $request['race'])
             ->first();
-            $crypted_password = passwordhash($request['password'], PASSWORD_BCRYPT);
+            $crypted_password = password_hash($request['password'], PASSWORD_BCRYPT);
             $email = $request['email'];
             $login = $request['login'];
             $user_id = DB::table('users')

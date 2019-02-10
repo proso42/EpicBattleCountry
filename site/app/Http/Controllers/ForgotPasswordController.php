833c7bb89757ba1bddd6431a482d3ed40fb9d38d
proso <proso@student.42.fr>
2019-02-10 19:54:11 +0100

    forgot password 1.1.12
    remove '\' in shell cmd

--- site/app/Http/Controllers/ForgotPasswordController.php
+++ site/app/Http/Controllers/ForgotPasswordController.php
@@ -28,7 +28,7 @@
                 $link = "http://www.epicbattlecorp.fr/reset_password?user_email=" . $email . "&reset_token=" . $reset_token;
                 DB::table('reseting_password')
                 ->insert(['user_id' => $user->id, 'user_email' => $email, 'reset_token' => $reset_token, 'status' => 'Waiting']);
-                $cmd = "cd /home/boss/www/scripts ; node ./send_reset_password_email.js " . $email  . "\'" . $link . "\'";
+                $cmd = "cd /home/boss/www/scripts ; node ./send_reset_password_email.js " . $email  . "'" . $link . "'";
                 exec($cmd);
                 return view('send_reset_password_email', compact('email'));
             }

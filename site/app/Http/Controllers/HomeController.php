db9ffd541105a26f3c1605ab2b4bf76f849c918e
proso <proso@student.42.fr>
2019-03-19 19:29:29 +0100

    debug 1.0.6
    remove dd ;

--- site/app/Http/Controllers/HomeController.php
+++ site/app/Http/Controllers/HomeController.php
@@ -38,7 +38,6 @@
             ->where('owner', '=', $user_id)
             ->where('id', '=', $city_id)
             ->first();
-            dd($user_id);
             $food = $city->food;
             $compact_food = $food;
             $max_food = $city->max_food;

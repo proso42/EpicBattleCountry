7893e565c8147c22ca7f718007a2791cd45ce82f
proso <proso@student.42.fr>
2019-03-19 17:28:37 +0100

    buildings 1.11.2
    change defensive to religious in compact ;

--- site/app/Http/Controllers/BuildingsController.php
+++ site/app/Http/Controllers/BuildingsController.php
@@ -67,7 +67,7 @@
             $allowed_army_buildings = $this->get_allowed_buildings('army_buildings');
             $allowed_religious_buildings = $this->get_allowed_buildings('religious_buildings');
             $allowed_tech_buildings = $this->get_allowed_buildings('tech_buildings');
-            return view('buildings', compact('first_active_tab', 'food', 'compact_food', 'max_food', 'wood', 'compact_wood' ,'max_wood', 'rock', 'compact_rock', 'max_rock', 'steel', 'compact_steel', 'max_steel', 'gold', 'compact_gold', 'max_gold', 'allowed_eco_buildings', 'allowed_army_buildings', 'allowed_defensive_buildings', 'allowed_tech_buildings'));
+            return view('buildings', compact('first_active_tab', 'food', 'compact_food', 'max_food', 'wood', 'compact_wood' ,'max_wood', 'rock', 'compact_rock', 'max_rock', 'steel', 'compact_steel', 'max_steel', 'gold', 'compact_gold', 'max_gold', 'allowed_eco_buildings', 'allowed_army_buildings', 'allowed_religious_buildings', 'allowed_tech_buildings'));
         }
 
         private function get_allowed_buildings($building_type)

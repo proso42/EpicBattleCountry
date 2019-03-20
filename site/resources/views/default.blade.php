df73431590fbfe1dff7d5ad8abe094befaf4485b
proso <proso@student.42.fr>
2019-03-20 17:45:26 +0100

    default page
    add default page for menu top and menu-left ;
    try to use it on home page ;

---
+++ site/resources/views/default.blade.php
@@ -0,0 +1,83 @@
+<div class="menu-top">
+            <div class="row">
+                <div class="col-lg-1 col-md-1 col-sm-1 col-0"></div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-3">
+                    <img style="margin-top: 15px;" src="images/swords.png">
+                </div>
+                <div class="col-lg-4 col-md-4 col-sm-4 col-6">
+                    <h1 style="margin-top: 25px;">EpicBattle</h1>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-3">
+                    <img style="margin-top: 15px;" src="images/swords.png">
+                </div>
+            </div>
+            <div style="margin-top: 25px;">
+                <div class="row">
+                    <div class="offset-lg-1 offset-md-1 offset-sm-1 offset-1 col-lg-2 col-md-2 col-sm-2 col-2">
+                        <div class="row">
+                            <div  class="col-lg-12" title="Food : {{ $food }}">
+                                <img src="images/food.png">
+                                <span id="food" style="margin-left: 5px;margin-top: 7px;margin-right: 10px; @if ($food == $max_food) color:maroon @elseif ($food >= ($max_food / 10 * 9)) color:darkorange @endif">
+                                    {{ $food }}
+                                </span>
+                                <span id="compact_food" style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($food == $max_food) color:maroon @elseif ($food >= ($max_food / 10 * 9)) color:darkorange @endif; display: none">
+                                    {{ $compact_food }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-2">
+                        <div class="row">
+                            <div  class="col-lg-12" title="Wood : {{ $wood }}">
+                                <img src="images/wood.png">
+                                <span style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($wood == $max_wood) color:maroon @elseif ($wood >= ($max_wood / 10 * 9)) color:darkorange @endif">
+                                    {{ $wood }}
+                                </span>
+                                <span id="compact_wood" style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($wood == $max_wood) color:maroon @elseif ($wood >= ($max_wood / 10 * 9)) color:darkorange @endif; display: none">
+                                    {{ $compact_wood }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-2">
+                        <div class="row">
+                            <div  class="col-lg-12" title="Rock : {{ $rock }}">
+                                <img src="images/rock.png">
+                                <span style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($rock == $max_rock) color:maroon @elseif ($rock >= ($max_rock / 10 * 9)) color:darkorange @endif">
+                                    {{ $rock }}
+                                </span>
+                                <span id="compact_rock" style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($rock == $max_rock) color:maroon @elseif ($rock >= ($max_rock / 10 * 9)) color:darkorange @endif; display: none">
+                                    {{ $compact_rock }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-2">
+                        <div class="row">
+                            <div  class="col-lg-12" title="Steel : {{ $steel }}">
+                                <img src="images/steel.png">
+                                <span style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($steel == $max_steel) color:maroon @elseif ($steel >= ($max_steel / 10 * 9)) color:darkorange @endif">
+                                    {{ $steel }}
+                                </span>
+                                <span id="compact_steel" style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($steel == $max_steel) color:maroon @elseif ($steel >= ($max_steel / 10 * 9)) color:darkorange @endif; display: none">
+                                    {{ $compact_steel }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-2 col-sm-2 col-2">
+                        <div class="row">
+                            <div  class="col-lg-12" title="Gold : {{ $gold }}">
+                                <img src="images/gold.png">
+                                <span style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($gold == $max_gold) color:maroon @elseif ($gold >= ($max_gold / 10 * 9)) color:darkorange @endif">
+                                    {{ $gold }}
+                                </span>
+                                <span id="compact_gold" style="margin-left: 5px;margin-top: 7px;margin-right: 10px;@if ($gold == $max_gold) color:maroon @elseif ($gold >= ($max_gold / 10 * 9)) color:darkorange @endif; display: none">
+                                    {{ $compact_gold }}
+                                </span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>

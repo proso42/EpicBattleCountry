a2f34cf7f812358ca72e74d7a1062355c8bd20ad
proso <proso@student.42.fr>
2019-03-22 16:49:57 +0100

    home infos 1.3.12
    comment again  ;

--- site/app/Http/Controllers/InterruptController.php
+++ site/app/Http/Controllers/InterruptController.php
@@ -19,13 +19,13 @@
                 $table = "waiting_buildings";
             else
                 $table = "waiting_techs";
-            /*$elem_canceled = DB::table($table)
+            $elem_canceled = DB::table($table)
             ->where('id', '=', $wait_id)
             ->get();
-            $niv = $elem_canceled->next_level - 1;
+            /*$niv = $elem_canceled->next_level - 1;
             $basic_price = 0;
             $levelup_price = 0;
-            /*if ($type_canceled == "building")
+            if ($type_canceled == "building")
             {
                 $build_info = DB::table($elem_canceled->type)
                 ->where('id', '=', $elem_canceled->building_id)

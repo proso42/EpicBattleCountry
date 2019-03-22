fa99e984d9f302911b73e9faf11d516aeebc33a2
proso <proso@student.42.fr>
2019-03-22 16:45:00 +0100

    home infos 1.3.10
    re-comment code to find error ;

--- site/app/Http/Controllers/InterruptController.php
+++ site/app/Http/Controllers/InterruptController.php
@@ -25,7 +25,7 @@
             $niv = $elem_canceled->next_level - 1;
             $basic_price = 0;
             $levelup_price = 0;
-            if ($type_canceled == "building")
+            /*if ($type_canceled == "building")
             {
                 $build_info = DB::table($elem_canceled->type)
                 ->where('id', '=', $elem_canceled->building_id)
@@ -89,7 +89,7 @@
             ->update(['food' => $food_refund, 'wood' => $wood_refund, 'rock' => $rock_refund, 'steel' => $steel_refund, 'gold' => $gold_refund]);
             DB::table($table)
             ->where('id', '=', $wait_id)
-            ->delete();
+            ->delete();*/
         }
 
         private function get_exp_value($niv, $basic_value, $levelup)

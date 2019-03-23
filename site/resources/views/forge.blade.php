038e23542230b3ea6b9f69137f90aa28e7e05be5
proso <proso@student.42.fr>
2019-03-23 21:27:29 +0100

    forge 1.2.2
    remove table;

--- site/resources/views/forge.blade.php
+++ site/resources/views/forge.blade.php
@@ -16,38 +16,6 @@
                     @foreach ($allowed_items as $item)
                         <div id="id_{{ $item['name'] }}" class="row" style="align-items: baseline;">
                             <span class="col-lg-2 col-md-2 col-sm-2 col-2">{{ $item['name'] }}</span><input type="text" placeholder="quantité" class="col-lg-2 col-md-2 col-sm-2 col-2 forge-input">
-                            <table class="col-lg-6 col-md-6 col-sm-6 col-6 forge-table-res">
-                                <tr>
-                                    @if ($item['food_required'] > 0)
-                                        <td>
-                                            <img class="forge-image" src="images/food.png"> x{{ $item['food_required'] }}
-                                        </td>
-                                    @endif
-                                    @if ($item['wood_required'] > 0)
-                                        <td>
-                                            <img class="forge-image" src="images/wood.png"> x{{ $item['wood_required'] }}
-                                        </td>
-                                    @endif
-                                    @if ($item['rock_required'] > 0)
-                                        <td>
-                                            <img class="forge-image" src="images/rock.png"> x{{ $item['rock_required'] }}
-                                        </td>
-                                    @endif
-                                    @if ($item['steel_required'] > 0)
-                                        <td>
-                                            <img class="forge-image"src="images/steel.png"> x{{ $item['steel_required'] }}
-                                        </td>
-                                    @endif
-                                    @if ($item['gold_required'] > 0)
-                                        <td>
-                                            <img class="forge-image" src="images/gold.png"> x{{ $item['gold_required'] }}
-                                        </td>
-                                    @endif
-                                    <td>
-                                        <i class="fas fa-clock icon"></i>{{ $item['duration'] }}
-                                    </td>
-                                </tr>
-                            </table>
                             <div class="col-lg-2 col-md-2 col-sm-2 col-2 forge-button">Produire</div>
                         </div>
                     @endforeach

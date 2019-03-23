bc5a63449e120f94e896aaffb0243e2d858aed22
proso <proso@student.42.fr>
2019-03-23 19:42:39 +0100

    buildings 3.1.6
    check $building_id;

--- site/app/Http/Controllers/BuildingsController.php
+++ site/app/Http/Controllers/BuildingsController.php
@@ -265,7 +265,7 @@
             ->value('id');
             if ($alreday_waiting !== null && $alreday_waiting > 0)
                 return ("already waiting");
-            return ($building_type);
+            return ($building_id);
             $building_info = DB::table($building_type)
             ->select('race_required', 'building_required', 'tech_required', 'basic_price', 'levelup_price', 'duration')
             ->where('id', '=', $building_id)

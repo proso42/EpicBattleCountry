f4953c358266b087ccce97ae0db3825a2e9186ae
proso <proso@student.42.fr>
2019-03-23 19:05:44 +0100

    buildings 3.0.2
    return a message in function of error ;
    print the msg in js ;

--- site/resources/views/buildings.blade.php
+++ site/resources/views/buildings.blade.php
@@ -290,14 +290,15 @@
                 {
                     if (xhr.readyState === 4 && xhr.status === 200)
                     {
-                        window.location.href="http://www.epicbattlecorp.fr/buildings?activeTab=" + (activeTab.split('-'))[0];
+                        //window.location.href="http://www.epicbattlecorp.fr/buildings?activeTab=" + (activeTab.split('-'))[0];
+                        console.log(xhr.responseText);
                     }
                 }
                 xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
                 xhr.send('_token=' + _token + '&building_name=' + name);
-                setTimeout(() => {
+                /*setTimeout(() => {
                     window.location.href="http://www.epicbattlecorp.fr/buildings?activeTab=" + (activeTab.split('-'))[0];
-                }, 500);
+                }, 500);*/
             }
         </script>
     </body>

c28fec3c723282a56ac31e2c96dcaf6ccaf8f395
proso <proso@student.42.fr>
2019-03-23 22:34:29 +0100

    forge 1.3.1
    css ;

--- site/resources/views/forge.blade.php
+++ site/resources/views/forge.blade.php
@@ -17,7 +17,7 @@
                         <div id="id_{{ $item['name'] }}" class="row" style="align-items: baseline;line-height: 31px;">
                             <span class="offset-lg-2 offset-md-2 offset-sm-2 offset-2 col-lg-2 col-md-2 col-sm-2 col-2" style="text-align:center">{{ $item['name'] }}</span>
                             <input type="text" placeholder="Quantité" class="settings-input col-lg-2 col-md-2 col-sm-2 col-2">
-                            <div class="forge-ressources">
+                            <div class="forge-ressources col-lg-2 col-md-2 col-sm-2 col-2">
                                 @if ($item['food_required'] > 0)
                                     <img src="images/food.png"> x{{ $item['food_required'] }}
                                 @endif

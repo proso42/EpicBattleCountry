a48b40d14cf9fcabb6799879a24a79ccc6715a41
proso <proso@student.42.fr>
2019-03-24 21:21:21 +0100

    army 1.0.1
    add onclick o redirect on army page in menu left ;

--- site/resources/views/default.blade.php
+++ site/resources/views/default.blade.php
@@ -103,7 +103,7 @@
             <div class="col-lg-1 col-md-1 col-sm-1 col-1"><i class="fas fa-map-marked-alt icon"></i></div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-3">Exploration</div>
         </div>
-        <div class="row menu-left">
+        <div onclick="document.location.href='/army'" class="row menu-left">
             <div class="col-lg-1 col-md-1 col-sm-1 col-1"><i class="fas fa-chess-rook icon"></i></div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-3">Armée</div>
         </div>

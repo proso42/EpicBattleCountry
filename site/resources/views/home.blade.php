5a84cae2446fc9be029c0773df329fa243d0bc87
proso <proso@student.42.fr>
2019-03-24 19:38:45 +0100

    home infos forge 1.0.3
    fix missing '}' ;

--- site/resources/views/home.blade.php
+++ site/resources/views/home.blade.php
@@ -50,7 +50,7 @@
                                         <i class="fas fa-cog icon"></i>
                                     @endif
                                 </div>
-                                <span style="color: gold">@if($elem['type'] == 'item') {{ $elem['name'] }} x{{ $elem['quantity'] } @else {{ $elem['name'] }} @endif}</span>
+                                <span style="color: gold">@if($elem['type'] == 'item') {{ $elem['name'] }} x{{ $elem['quantity'] }} @else {{ $elem['name'] }} @endif}</span>
                                 <span id="compteur_{{ $elem['name'] }}" duration="{{ $elem['duration'] }}" name="{{ $elem['name'] }}" @if($elem['type'] == 'item') quantity="x{{ $elem['quantity'] }}" @endif class="col-lg-8 col-md-8 col-sm-8 col-8 infos-building-wip"></span>
                                 <div id="interrupt_{{ $elem['name'] }}" class="col-lg-2 col-md-2 col-sm-2 col-2">
                                     <i title="Interrompre" onclick="interrupt('{{ $elem['wait_id'] }}', '{{ $elem['type'] }}', 'id_{{ $elem['name'] }}')" class="fas fa-times icon-red"></i>

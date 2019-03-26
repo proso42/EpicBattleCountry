c9c32c804a156a943bbee01e163e4c274468de8b
proso <proso@student.42.fr>
2019-03-26 15:27:47 +0100

    map 1.0.0
    add map link in menu-left ;
    juste create empty map page ;

---
+++ site/app/Http/Controllers/MapController.php
@@ -0,0 +1,19 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use App\Http\Requests;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Session;
+
+    class MapController extends Controller
+    {
+        public function index()
+        {
+            return view('map');
+        }
+    }
+
+?>
--- site/resources/views/default.blade.php
+++ site/resources/views/default.blade.php
@@ -123,7 +123,7 @@
             <div class="col-lg-1 col-md-1 col-sm-1 col-1"><i class="fas fa-shield-alt icon"></i></div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-3">Alliance</div>
         </div>
-        <div class="row menu-left">
+        <div onclick="document.location.href='/map'" class="row menu-left">
             <div class="col-lg-1 col-md-1 col-sm-1 col-1"><i class="fas fa-globe-americas icon"></i></div>
             <div class="col-lg-3 col-md-3 col-sm-3 col-3">Carte</div>
         </div>

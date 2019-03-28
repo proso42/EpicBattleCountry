ff2f618dec804e363becfc46230e5389193ea21f
proso <proso@student.42.fr>
2019-03-28 16:27:21 +0100

    map 1.4.0
    add protection if player has no cartographer ;

--- site/resources/views/map.blade.php
+++ site/resources/views/map.blade.php
@@ -10,40 +10,44 @@
     <body>
         @include('default')
             <div class="offset-lg-0 offset-md-2 offset-sm-1 offset-1 col-lg-9 col-md-7 col-sm-10 col-10 center-win" style="margin-top: 50px; padding-right: 10px;">
-                @if ($move_map == 1)
-                    <i class="fas fa-arrow-circle-left" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x + 1}}, {{ $y_pos - $city_y}})"></i>
-                    <i class="fas fa-arrow-circle-up" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x}}, {{ $y_pos - $city_y + 1 }})"></i>
-                    <i class="fas fa-arrow-circle-down" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x}}, {{ $y_pos - $city_y - 1 }})"></i>
-                    <i class="fas fa-arrow-circle-right" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x - 1}}, {{ $y_pos - $city_y}})"></i>
+                @if ($cartographer == 0)
+                    <p>Vous devez construire nu Cartoraphe pour pouvoir utiliser la carte !</p>
+                @else
+                    @if ($move_map == 1)
+                        <i class="fas fa-arrow-circle-left" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x + 1}}, {{ $y_pos - $city_y}})"></i>
+                        <i class="fas fa-arrow-circle-up" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x}}, {{ $y_pos - $city_y + 1 }})"></i>
+                        <i class="fas fa-arrow-circle-down" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x}}, {{ $y_pos - $city_y - 1 }})"></i>
+                        <i class="fas fa-arrow-circle-right" style="cursor: pointer" onclick="move_map({{ $x_pos - $city_x - 1}}, {{ $y_pos - $city_y}})"></i>
+                    @endif
+                    <br/>
+                    <div style="display: inline-block">
+                        <table class="map-table">
+                            <?php $i=0; ?>
+                            @for ($y = $y_pos + $cartographer; $y >= $y_pos - $cartographer; $y--)
+                                <tr>
+                                    @for ($x = $x_pos - $cartographer; $x <= $x_pos + $cartographer; $x++)
+                                        @if ($i < count($visible_cells) && $x == $visible_cells[$i]['x_pos'] && $y == $visible_cells[$i]['y_pos'])
+                                            <td title="{{ $visible_cells[$i]['x_pos'] }}/{{ $visible_cells[$i]['y_pos'] }} {{ $visible_cells[$i]['title'] }}" class="map-cell" style="background-color: {{ $visible_cells[$i]['background-color'] }}"><i class="fas {{ $visible_cells[$i]['class'] }}"></i></td>
+                                            <?php $i++; ?>
+                                        @else
+                                            <td title="{{ $x }}/{{ $y }}" class="map-empty-cell"><i class="fas fa-road"></i></td>
+                                        @endif
+                                    @endfor
+                                </tr>
+                            @endfor
+                        </table>
+                    </div>
+                    <br/>
+                    
+                    <hr class="signin-footer">
+                    <ul style="text-align: left">
+                        <li><i class="fas fa-star"></i> Votre ville</li>
+                        <li><i class="fas fa-city"></i> Autre ville</li>
+                        <li><i class="fas fa-dungeon"></i> Ruine</li>
+                        <li><i class="fas fa-dragon"></i> Tannière d'un dragon</li>
+                        <li><i class="fas fa-water"></i> Fleuve / Lac</li>
+                    </ul>
                 @endif
-                <br/>
-                <div style="display: inline-block">
-                    <table class="map-table">
-                        <?php $i=0; ?>
-                        @for ($y = $y_pos + $cartographer; $y >= $y_pos - $cartographer; $y--)
-                            <tr>
-                                @for ($x = $x_pos - $cartographer; $x <= $x_pos + $cartographer; $x++)
-                                    @if ($i < count($visible_cells) && $x == $visible_cells[$i]['x_pos'] && $y == $visible_cells[$i]['y_pos'])
-                                        <td title="{{ $visible_cells[$i]['x_pos'] }}/{{ $visible_cells[$i]['y_pos'] }} {{ $visible_cells[$i]['title'] }}" class="map-cell" style="background-color: {{ $visible_cells[$i]['background-color'] }}"><i class="fas {{ $visible_cells[$i]['class'] }}"></i></td>
-                                        <?php $i++; ?>
-                                    @else
-                                        <td title="{{ $x }}/{{ $y }}" class="map-empty-cell"><i class="fas fa-road"></i></td>
-                                    @endif
-                                @endfor
-                            </tr>
-                        @endfor
-                    </table>
-                </div>
-                <br/>
-                
-                <hr class="signin-footer">
-                <ul style="text-align: left">
-                    <li><i class="fas fa-star"></i> Votre ville</li>
-                    <li><i class="fas fa-city"></i> Autre ville</li>
-                    <li><i class="fas fa-dungeon"></i> Ruine</li>
-                    <li><i class="fas fa-dragon"></i> Tannière d'un dragon</li>
-                    <li><i class="fas fa-water"></i> Fleuve / Lac</li>
-                </ul>
             </div>
         </div>
         <script>

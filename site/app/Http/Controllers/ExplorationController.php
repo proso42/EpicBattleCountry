3c09f6979e1a8c40203dd28b9d83d9e6e8045240
proso <proso@student.42.fr>
2019-04-03 21:25:10 +0200

    exploration 1.2.23
    wtf;

--- site/app/Http/Controllers/ExplorationController.php
+++ site/app/Http/Controllers/ExplorationController.php
@@ -126,7 +126,7 @@
             else
                 $unit = "Eclaireur";
             $unit_avaible = DB::table('cities_units')->where('city_id', '=', $city_id)->value($unit);
-            $city_res = DB::table('cities')->select('food', 'wood', 'rock', 'steel', 'gold', 'x_pos', 'y_pos')->where('id', '=', $city_id)->get();
+            $city_res = DB::table('cities')->select('food', 'wood', 'rock', 'steel', 'gold', 'x_pos', 'y_pos')->where('id', '=', $city_id)->first();
             $unit_required = 1;
             $food_required = 100;
             $wood_required = 0;
@@ -141,9 +141,8 @@
                 $steel_required = 2500;
                 $gold_required = 1000;
             }
-            return ("food : " . $city_res->food);
-            /*if ($unit_avaible < $unit_required || $food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold)
-                return 1;*/
+            if ($food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold)
+                return 1;
             $speed = 3600 / (DB::table('unit')->where('name', '=', $unit)->value('speed'));
             $finishing_date = sec_to_date((abs($city_res->x_pos - $dest_x) + abs($city_res->y_pos - $dest_y)) * $speed);
             $cartographe = DB::table('cities_buildings')->where('city_id', '=', $city_id)->value('Cartographe');

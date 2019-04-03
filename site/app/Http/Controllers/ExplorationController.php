5f0f1d91ccc6af0a08d7ce6f01612a7627fd3c34
proso <proso@student.42.fr>
2019-04-03 21:46:30 +0200

    exploration 1.2.31
    wtf ;

--- site/app/Http/Controllers/ExplorationController.php
+++ site/app/Http/Controllers/ExplorationController.php
@@ -144,8 +144,9 @@
             if ($unit_avaible < $unit_required || $food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold)
                 return 1;
             $speed = 3600 / (DB::table('units')->where('name', '=', $unit)->value('speed'));
-            return $speed;
-            $finishing_date = sec_to_date((abs($city_res->x_pos - $dest_x) + abs($city_res->y_pos - $dest_y)) * $speed);
+            $total_time = (abs($city_res->x_pos - $dest_x) + abs($city_res->y_pos - $dest_y) * $speed);
+            return $total_time;
+            $finishing_date = sec_to_date($total_time);
             /*$cartographe = DB::table('cities_buildings')->where('city_id', '=', $city_id)->value('Cartographe');
             $x_min = $city_res->x_pos - $cartographe;
             $x_max = $city_res->x_pos + $cartographe;

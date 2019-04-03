41baa75d637f8e6549c342d01d918218dda55923
proso <proso@student.42.fr>
2019-04-03 21:18:38 +0200

    exploration 1.2.19
    use try catch to get the wtf error msg ;

--- site/app/Http/Controllers/ExplorationController.php
+++ site/app/Http/Controllers/ExplorationController.php
@@ -141,8 +141,15 @@
                 $steel_required = 2500;
                 $gold_required = 1000;
             }
-            if ($unit_avaible < 2)
-                return 1;
+            try
+            {
+                if ($unit_avaible < 2)
+                    echo "WTF";
+            }
+            catch (Exception $e)
+            {
+                return ($e->getMessage());
+            }
             /*if ($unit_avaible < $unit_required || $food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold)
                 return 1;*/
             $speed = 3600 / (DB::table('unit')->where('name', '=', $unit)->value('speed'));

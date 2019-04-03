257820738231a0502ea9c10f2c8a46674cd41a11
proso <proso@student.42.fr>
2019-04-03 21:08:20 +0200

    exploration 1.2.11
    control $unit_avaible ;

--- site/app/Http/Controllers/ExplorationController.php
+++ site/app/Http/Controllers/ExplorationController.php
@@ -141,6 +141,7 @@
                 $steel_required = 2500;
                 $gold_required = 1000;
             }
+            return $unit_avaible;
             if ($unit_avaible < $unit_required/* || $food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold*/)
                 return 1;
             $speed = 3600 / (DB::table('unit')->where('name', '=', $unit)->value('speed'));

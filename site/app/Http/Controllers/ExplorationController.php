ec81f81bcc476254151bc67a97aa512479024686
proso <proso@student.42.fr>
2019-04-03 21:06:02 +0200

    exploration 1.2.10
    basic condition make a wtf error ;
    comment part of condition, keep just first test ;

--- site/app/Http/Controllers/ExplorationController.php
+++ site/app/Http/Controllers/ExplorationController.php
@@ -141,7 +141,7 @@
                 $steel_required = 2500;
                 $gold_required = 1000;
             }
-            /*if ($unit_avaible < $unit_required || $food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold)
+            if ($unit_avaible < $unit_required/* || $food_required > $city_res->food || $wood_required > $city_res->wood || $rock_required > $city_res->rock || $steel_required > $city_res->steel || $gold_required > $city_res->gold*/)
                 return 1;
             $speed = 3600 / (DB::table('unit')->where('name', '=', $unit)->value('speed'));
             $finishing_date = sec_to_date((abs($city_res->x_pos - $dest_x) + abs($city_res->y_pos - $dest_y)) * $speed);
@@ -153,7 +153,7 @@
             if ($dest_x < $x_min || $dest_x > $x_max || $dest_y < $y_min || $dest_y > $y_max)
                 return ($finishing_date . ";warning");
             else
-                return ($finishing_date . ";easy");*/
+                return ($finishing_date . ";easy");
         }
     }
 ?>

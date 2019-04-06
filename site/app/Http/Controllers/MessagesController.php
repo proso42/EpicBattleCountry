dd7029b5112ac166fd33843d0cdc3e01a9245ce1
proso <proso@student.42.fr>
2019-04-06 23:53:07 +0200

    msg 1.2.1
    fix laravel's bullshit ;

--- site/app/Http/Controllers/MessagesController.php
+++ site/app/Http/Controllers/MessagesController.php
@@ -72,8 +72,8 @@
             $notifications = [];
             $msg_sended = [];
             $msg_received = [];
-            $all_user_msgs = DB::table('messages')->where('target', '=', $user_id)->where('sender', '=', $user_id)->get();
-            dd($all_user_msgs);
+            $all_user_msgs = DB::table('messages')->where('target', '=', $user_id)->orWhere('sender', '=', $user_id)->get();
+            //dd($all_user_msgs);
             $all_users = DB::table('users')->get();
             $notif_alert = 0;
             $msg_received_alert = 0;

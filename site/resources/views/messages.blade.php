8f63a168c0fbc9abe3dd3ec8afc79fa75f72d19e
proso <proso@student.42.fr>
2019-04-06 23:37:21 +0200

    msg 1.0.10
    wtf ;

--- site/resources/views/messages.blade.php
+++ site/resources/views/messages.blade.php
@@ -10,6 +10,20 @@
     <body>
         @include('default')
             <div class="offset-lg-0 offset-md-2 offset-sm-1 offset-1 col-lg-9 col-md-7 col-sm-10 col-10 center-win" style="margin-top: 50px; padding-right: 10px;">
+                <div class="row">
+                    <div id="notif-tab" class="col-lg-3 col-md-3 col-sm-3 col-3 generique-tab" onclick="switchTab('notif')">
+                        @if ($notif_alert > 0)<i id="notif_alert" class="fas fa-exclamation-circle icon-color-red"></i> @endif Notifications (<span id="nb_notif" @if($notif_alert == 0) style="color: ligthyellow" @else style="color: crimson">{{ $notif_alert }}</span>)
+                    </div>
+                    <div id="sended-tab" class="col-lg-3 col-md-3 col-sm-3 col-3 generique-tab" onclick="switchTab('sended')">
+                        Messages envoyés                        
+                    </div>
+                    <div id="received-tab" class="col-lg-3 col-md-3 col-sm-3 col-3 generique-tab" onclick="switchTab('received')">
+                        @if ($msg_received_alert > 0)<i id="msg_received_alert" class="fas fa-exclamation-circle icon-color-red"></i> @endif Messages reçus (<span id="nb_msg_received" @if($msg_received_alert == 0) style="color: ligthyellow" @else style="color: crimson">{{ $msg_received_alert }}</span>)                     
+                    </div>
+                    <div id="blocked-tab" class="col-lg-3 col-md-3 col-sm-3 col-3 generique-tab" onclick="switchTab('blocked')">
+                        Joueurs bloqués       
+                    </div>
+                </div>
             </div>
         </div>
         <input id="_token" name="_token" type="hidden" value="{{csrf_token()}}">

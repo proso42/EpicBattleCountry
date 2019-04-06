e1eba12df2fc52f58aa52b07ccf889492c03be7e
proso <proso@student.42.fr>
2019-04-06 23:36:20 +0200

    msg 1.0.9
    wtf ;

--- site/resources/views/messages.blade.php
+++ site/resources/views/messages.blade.php
@@ -12,6 +12,8 @@
             <div class="offset-lg-0 offset-md-2 offset-sm-1 offset-1 col-lg-9 col-md-7 col-sm-10 col-10 center-win" style="margin-top: 50px; padding-right: 10px;">
             </div>
         </div>
+        <input id="_token" name="_token" type="hidden" value="{{csrf_token()}}">
+        <div id="fat" style="display: none" first_active_tab="{{ $first_active_tab }}" ></div>
         <script>
             
             var activeTab = document.getElementById("fat").getAttribute("first_active_tab") + "-tab";

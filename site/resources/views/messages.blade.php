54b1a2ee9ecc989bd6faa7b8f8ccbdbf333a9132
proso <proso@student.42.fr>
2019-04-07 16:28:44 +0200

    msg 1.2.9
    fix bad css class name ;

--- site/resources/views/messages.blade.php
+++ site/resources/views/messages.blade.php
@@ -137,9 +137,9 @@
                     document.getElementById('content_' + id).style.display = "";
                     eye.className = "offset-lg-1 offset-md-1 offset-sm-1 offset-1 col-lg-1 col-md-1 col-sm-1 col-1 fas fa-eye-slash";
                     icon_envelope = document.getElementById('seen_' + id);
-                    if (icon_envelope.className = "offset-lg-1 offset-md-1 offset-sm-1 offset-1 col-lg-1 col-md-1 col-sm-1 col-1 fas fa-envelope icon-color-red")
+                    if (icon_envelope.className = "col-lg-1 col-md-1 col-sm-1 col-1 fas fa-envelope icon-color-red")
                     {
-                        icon_envelope.className = "offset-lg-1 offset-md-1 offset-sm-1 offset-1 col-lg-1 col-md-1 col-sm-1 col-1 fas fa-envelope-open-text icon-color-yellow";
+                        icon_envelope.className = "col-lg-1 col-md-1 col-sm-1 col-1 fas fa-envelope-open-text icon-color-yellow";
                         var _token = document.getElementById("_token").value;
                         var xhr = new XMLHttpRequest();
                         xhr.open('POST', 'http://www.epicbattlecorp.fr/seen_msg');
@@ -148,7 +148,6 @@
                             if (xhr.readyState === 4 && xhr.status === 200)
                             {
                                 console.log('ok !');
-                                if (icon_envelope.className = "offset-lg-1 offset-md-1 offset-sm-1 offset-1 col-lg-1 col-md-1 col-sm-1 col-1 fas fa-envelope icon-color-red") 
                                     decrease(type);
                             }
                         }

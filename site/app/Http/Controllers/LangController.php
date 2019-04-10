c720e7cb93f4d7b323e2ebe32aa4e2a0b77b2c74
proso <proso@student.42.fr>
2019-04-10 19:56:50 +0200

    lang 1.0.3
    try with setLocale ;

--- site/app/Http/Controllers/LangController.php
+++ site/app/Http/Controllers/LangController.php
@@ -7,18 +7,19 @@
     use Illuminate\Support\Facades\DB;
     use Illuminate\Support\Facades\Auth;
     use Illuminate\Support\Facades\Session;
+    use Illuminate\Support\Facades\App;
 
     class LangController extends Controller
     {
         public function switch_fr()
         {
-            session(['lang' => 'fr']);
+            App::setLocale('fr');
             return redirect('/settings');
         }
 
         public function switch_en()
         {
-            session(['lang' => 'en']);
+            App::setLocale('en');
             return redirect('/settings');
         }
     }

9854155728fe179ab7191d9918fdac1237444a82
proso <proso@student.42.fr>
2019-04-11 19:02:11 +0200

    lang settings 1.0.0
    add few trad for settings ;

---
+++ site/resources/lang/fr/settings.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Settings Language Lines EN
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'reseting_passwd' => 'Reinitialization of password',
+    'error_not_same_passwd' => 'The two passwords are not identical !',
+    'passwd' => 'Password',
+    'confirm_passwd' => 'Confirm password',
+    'passwd_update' => 'Change password',
+    'passwd_changed' => 'Your password has been successfully changed !',
+    'error_bad_current_passwd' => 'The current password is not good !',
+    'error_same_current_and)new_passwd' => 'The new password can not be the current password !',
+    'current_passwd' => 'Current password',
+    'confirm_new_passwd' => 'Confirm new password',
+];

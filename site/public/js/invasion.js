2760b24e2f493609e5c5114d5e50c51b9a0cfef6
proso <proso@student.42.fr>
2019-04-20 18:00:35 +0200

    invasion [js fix] [WIP]

--- site/public/js/invasion.js
+++ site/public/js/invasion.js
@@ -1,8 +1,5 @@
-console.log("oui");
-
-function hid_img(side)
+function hide_img(side)
 {
-    console.log("hide_img");
     if (side == "left")
         document.getElementsByClassName("invasion-left-overlay").style.display = "";
     else if (side == "right")

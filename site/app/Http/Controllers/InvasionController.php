46a4fca75ec4fb5799b124563f8836aad81b8bdc
proso <proso@student.42.fr>
2019-04-20 16:36:49 +0200

    Invasion [create] [WIP]

---
+++ site/app/Http/Controllers/InvasionController.php
@@ -0,0 +1,76 @@
+<?php
+
+    namespace App\Http\Controllers;
+
+    use App\Http\Requests;
+    use Illuminate\Http\Request;
+    use Illuminate\Support\Facades\DB;
+    use Illuminate\Support\Facades\Auth;
+    use Illuminate\Support\Facades\Session;
+
+    class InvasionController extends Controller
+    {
+        public function index()
+        {
+            $user_id = session()->get('user_id');
+            $user_race = session()->get('user_race');
+            if ($user_race === null)
+            {
+                $user_race = DB::table('users')
+                ->where('id', '=', $user_id)
+                ->value('race');
+                session()->put(['user_race' => $user_race]);
+            }
+            $city_id = session()->get('city_id');
+            if ($city_id === null)
+            {
+                $city_id = DB::table('cities')
+                ->where('owner', '=', $user_id)
+                ->where('is_capital', '=', 1)
+                ->value('id');
+                session()->put(['city_id' => $city_id]);
+            }
+            $is_admin = DB::table('users')->where('id', '=', $user_id)->value("is_admin");
+            $city = DB::table('cities')
+            ->where('owner', '=', $user_id)
+            ->where('id', '=', $city_id)
+            ->first();
+            $food = $city->food;
+            $compact_food = $food;
+            $max_food = $city->max_food;
+            $wood = $city->wood;
+            $compact_wood = $wood;
+            $max_wood = $city->max_wood;
+            $rock = $city->rock;
+            $compact_rock = $rock;
+            $max_rock = $city->max_rock;
+            $steel = $city->steel;
+            $compact_steel = $steel;
+            $max_steel = $city->max_steel;
+            $gold = $city->gold;
+            $compact_gold = $gold;
+            $max_gold = $city->max_gold;
+            if ($food > 999999)
+                $compact_food = substr($food, 0, 5) . '...';
+            if ($wood > 999999)
+                $compact_wood = substr($wood, 0, 5) . '...';
+            if ($rock > 999999)
+                $compact_rock = substr($rock, 0, 5) . '...';
+            if ($steel > 999999)
+                $compact_steel = substr($steel, 0, 5) . '...';
+            if ($gold > 999999)
+                $compact_gold = substr($gold, 0, 5) . '...';
+            $city_unit = DB::table('cities_units')->where('city_id', '=', $city_id)->get();
+            $info_unit = [];
+            foreach ($city_unit as $unit => $val)
+            {
+                if ($val <= 0)
+                    continue ;
+                array_push($info_unit, ["ref" => $unit, "name" => trans("unit." . $unit), "quantity" => $val]);
+            }
+            dd($info_unit);
+            return view('buildings', compact('is_admin', 'food', 'compact_food', 'max_food', 'wood', 'compact_wood' ,'max_wood', 'rock', 'compact_rock', 'max_rock', 'steel', 'compact_steel', 'max_steel', 'gold', 'compact_gold', 'max_gold'));
+        }
+    }
+
+?>

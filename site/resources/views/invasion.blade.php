66208973227f38a7b48bf66794328dd05bba22cb
proso <proso@student.42.fr>
2019-04-20 20:59:34 +0200

    invasion [test]
    test with onmousedown

--- site/resources/views/invasion.blade.php
+++ site/resources/views/invasion.blade.php
@@ -25,8 +25,8 @@
                             <div id="unit_{{ $unit['ref'] }}" class="row invasion-unit-line" unit_ref="{{ $unit['ref'] }}">
                                 <span class="col-lg-5 col-md-5 col-sm-5 col-5" style="text-align: left">{{ $unit['name'] }}</span>
                                 <span id="{{ $unit['ref'] }}_selected" class="col-lg-4 col-md-4 col-sm-4 col-4"> 0/{{ $unit['quantity'] }}</span>
-                                <span class="col-lg-1 col-md-1 col-sm-1 col-1"><i onclick="add_unit('{{ $unit['ref'] }}', '{{ $unit['quantity'] }}')" class="fas fa-plus invasion-plus"></i></span>
-                                <span class="col-lg-1 col-md-1 col-sm-1 col-1"><i onclick="remove_unit('{{ $unit['ref'] }}', '{{ $unit['quantity'] }}')" class="fas fa-minus invasion-minus"></i></span>
+                                <span class="col-lg-1 col-md-1 col-sm-1 col-1"><i onmousedown="add_unit('{{ $unit['ref'] }}', '{{ $unit['quantity'] }}')" class="fas fa-plus invasion-plus"></i></span>
+                                <span class="col-lg-1 col-md-1 col-sm-1 col-1"><i onmousedown="remove_unit('{{ $unit['ref'] }}', '{{ $unit['quantity'] }}')" class="fas fa-minus invasion-minus"></i></span>
                             </div>
                         @endforeach
                         <input onclick="back_step1()" id="cancel_button_1" type="button" class="home-button-cancel" value="@lang('common.return')">

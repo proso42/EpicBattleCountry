f5677e55a287a4a45f4bb4588d76cedf653fe76a
proso <proso@student.42.fr>
2019-04-20 18:03:31 +0200

    invasion [fix bullshit] [WIP]

--- site/resources/views/invasion.blade.php
+++ site/resources/views/invasion.blade.php
@@ -12,11 +12,11 @@
             <div class="offset-lg-0 offset-md-2 offset-sm-1 offset-1 col-lg-9 col-md-7 col-sm-10 col-10 center-win" style="margin-top: 50px; padding: 0;">
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-6 col-6" onmouseover="hide_img('right')">
-                        <div id="left_overlay" class="invasion-left-overlay"></div>
+                        <div id="left_overlay" class="invasion-left-overlay" style="display: none"></div>
                         <img class="invasion-left-image" src="images/invasion_battle.jpg">
                     </div>
                     <div class="col-lg-6 col-md-6 col-sm-6 col-6" onmouseover="hide_img('left')">
-                        <div id="right_overlay" class="invasion-right-overlay"></div>
+                        <div id="right_overlay" class="invasion-right-overlay" style="display: none"></div>
                         <img class="invasion-right-image" src="images/invasion_moving.jpg">
                     </div>
                 </div>

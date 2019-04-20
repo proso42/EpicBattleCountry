b88f49cac3d910de7abb67b78352f1f261d87d3c
proso <proso@student.42.fr>
2019-04-20 16:49:41 +0200

    invasion [update] [WIP]
    add vue ;

--- site/app/Http/Controllers/InvasionController.php
+++ site/app/Http/Controllers/InvasionController.php
@@ -70,8 +70,7 @@
                     continue ;
                 array_push($info_unit, ["ref" => $unit, "name" => trans("unit." . $unit), "quantity" => $val]);
             }
-            dd($info_unit);
-            return view('buildings', compact('is_admin', 'food', 'compact_food', 'max_food', 'wood', 'compact_wood' ,'max_wood', 'rock', 'compact_rock', 'max_rock', 'steel', 'compact_steel', 'max_steel', 'gold', 'compact_gold', 'max_gold'));
+            return view('invasion', compact('is_admin', 'food', 'compact_food', 'max_food', 'wood', 'compact_wood' ,'max_wood', 'rock', 'compact_rock', 'max_rock', 'steel', 'compact_steel', 'max_steel', 'gold', 'compact_gold', 'max_gold'));
         }
     }
 

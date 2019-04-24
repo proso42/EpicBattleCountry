b1ada0c31a9a18dba7a4bef91ecafcb5d3474b72
proso <proso@student.42.fr>
2019-04-24 23:29:50 +0200

    invasion [debug]

--- site/public/js/invasion.js
+++ site/public/js/invasion.js
@@ -222,13 +222,14 @@ function step5()
     {
         if (xhr.readyState === 4 && xhr.status === 200)
         {
-            if (xhr.responseText.indexOf("error") >= 0)
-                conosle.log(xhr.responseText);
+            /*if (xhr.responseText.indexOf("error") >= 0)
+                console.log(xhr.responseText);
             else
                 document.getElementById("units_move_success").style.display = "";
             setTimeout(function(){
                 window.location.reload();
-            }, 3000);
+            }, 3000);*/
+            console.log(xhr.responseText);
         }
     }
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");

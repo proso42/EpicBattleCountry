275a8eae1743be329fe09438c8876112a1726146
proso <proso@student.42.fr>
2019-04-24 22:32:01 +0200

    invasion [debug]

--- site/app/Http/Controllers/InvasionController.php
+++ site/app/Http/Controllers/InvasionController.php
@@ -131,13 +131,13 @@
             $res = $request['res'];
             $res = explode(",", preg_replace('/[{}\"]/', '', $res));
             $tab_res = [];
-            return ("ok");
-            /*foreach ($res as $key)
+            foreach ($res as $key)
             {
                 $ex = explode(":", $key);
                 $tab_res[$ex[0]] = $ex[1];
             }
-            $city_target = $request['city_target'];
+            return ("ok");
+            /*$city_target = $request['city_target'];
             $user_id = session()->get('user_id');
             $city_id = session()->get('city_id');
             $city_target_info = DB::table('cities')->where('name', '=', $city_target)->where('owner', '=', $user_id)->first();

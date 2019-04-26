fbaf1cfe31bd7c692047de57737043c509e54e2f
proso <proso@student.42.fr>
2019-04-26 17:39:45 +0200

    script battle [test]

--- older_site/js/module_launch_battle.js
+++ older_site/js/module_launch_battle.js
@@ -96,7 +96,7 @@ module.exports.launch_battle = function (id)
                                 var tech_name = ret[0]['name'];
                                 var boost = ret[0]['boost'];
                                 if (tech_name.indexOf(" ") >= 0)
-                                    tech_name = tech_name.replace('/\s/gi', "_");
+                                    tech_name = tech_name.replace(/\s/gi, "_");
                                 console.log(`SELECT ${tech_name} FROM cities_techs WHERE city_id = ${city_id}`);
                                 mysqlClient.query(`SELECT ${tech_name} FROM cities_techs WHERE city_id = ${city_id}`, function (err, ret){
                                     if (err)

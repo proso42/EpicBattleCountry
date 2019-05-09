758577365a7aced1913cb43bb4a2518ef6d1c0ee
proso <proso@student.42.fr>
2019-05-09 15:38:39 +0200

    divinity [debug]

--- site/app/Http/Controllers/DivinityController.php
+++ site/app/Http/Controllers/DivinityController.php
@@ -81,6 +81,7 @@
                 $allowed_disaster = null;
                 $disaster_cool_down = $this->sec_to_date($disaster_cool_down);
             }
+            dd($allowed_disaster);
             return view('divinity', compact('is_admin', 'food', 'compact_food', 'max_food', 'wood', 'compact_wood' ,'max_wood', 'rock', 'compact_rock', 'max_rock', 'steel', 'compact_steel', 'max_steel', 'gold', 'compact_gold', 'max_gold', 'faith', 'divinity_active_tab', 'allowed_disaster', 'disaster_cool_down'));
         }
 

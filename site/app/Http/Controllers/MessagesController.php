589b101818c1760cb7cf6a4aadb69ad73f3765df
proso <proso@student.42.fr>
2019-05-10 15:28:10 +0200

    msg [fix]
    fix missing path to new model ;

--- site/app/Http/Controllers/MessagesController.php
+++ site/app/Http/Controllers/MessagesController.php
@@ -3,6 +3,7 @@
     namespace App\Http\Controllers;
 
     use App\Http\Requests;
+    use App\Models\Common;
     use Illuminate\Http\Request;
     use Illuminate\Support\Facades\DB;
     use Illuminate\Support\Facades\Auth;

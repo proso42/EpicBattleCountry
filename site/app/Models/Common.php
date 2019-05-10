98dffee1d74dfcf8a5239f3f073ca4c513acb7a8
proso <proso@student.42.fr>
2019-05-10 17:08:27 +0200

    model [get_exp_value : fix]
    add missing 'static' ;

--- site/app/Models/Common.php
+++ site/app/Models/Common.php
@@ -90,7 +90,7 @@
                 return ($duration . " j");
         }
 
-        public function get_exp_value($niv, $basic_value, $levelup)
+        public static function get_exp_value($niv, $basic_value, $levelup)
         {
             $final_value = intval($basic_value);
             for ($i = 1; $i <= $niv; $i++)

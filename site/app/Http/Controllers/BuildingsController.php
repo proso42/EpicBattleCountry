857c68d9c96429ca45e6b3fdb14c8f56b5dbc954
proso <proso@student.42.fr>
2019-05-10 17:01:08 +0200

    buildings [fix]
    fix bad name of var ;

--- site/app/Http/Controllers/BuildingsController.php
+++ site/app/Http/Controllers/BuildingsController.php
@@ -157,7 +157,7 @@
                     if ($status === "OK")
                     {
                         $duration = $this->boost_meca($val->duration, $city_id);
-                        $duration = Common::sec_to_date($this->get_exp_value($niv, $duration, $val->levelup));
+                        $duration = Common::sec_to_date($this->get_exp_value($niv, $duration, $val->levelup_price));
                         
                     }
                     else

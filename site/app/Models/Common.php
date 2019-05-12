9db13e3dffd03272ae0d919bdbc20370a792c98b
proso <proso@student.42.fr>
2019-05-12 18:09:13 +0200

    menu-left [update]
    add red exclamation point after messages button if player has some not seen msgs ;

--- site/app/Models/Common.php
+++ site/app/Models/Common.php
@@ -63,6 +63,11 @@
                 $util->compact_gold = substr($util->gold, 0, 5) . '...';
             else
                 $util->compact_gold = $util->gold;
+            $msg_not_seen = DB::table('messages')
+            ->where('target_city', '=', 1)
+            ->where('seen', '=', 0)
+            ->count();
+            $util->msg_not_seen = $msg_not_seen;
             return $util;
         }
 

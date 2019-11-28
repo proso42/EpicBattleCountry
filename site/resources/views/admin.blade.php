7d6a09508341f42419a6a6398c9cf9bbc5e21e84
Philippe <rosophilippe@gmail.com>
2019-11-28 12:23:46 +0100

    admin [style]

--- site/resources/views/admin.blade.php
+++ site/resources/views/admin.blade.php
@@ -11,13 +11,15 @@
         @include('default')
             <div class="offset-lg-0 offset-md-2 offset-sm-1 offset-1 col-lg-9 col-md-7 col-sm-10 col-10 center-win" style="margin-top: 50px; padding-right: 10px;">
                 <div class="row" style="align-items: baseline;line-height: 31px;">
-                    <select class="admin-select offset-lg-2 offset-md-2 offset-sm-2 offset-2 col-lg-2 col-md-2 col-sm-2 col-2">
-                        <optgroup style="background-color: white" label="@lang('common.users')">
-                            @foreach ($all_users as $user)
-                                <option class="admin-option">{{ $user->login }}</option>
-                            @endforeach
-                        </optgroup>
-                    </select>
+                    <div>
+                        <select class="admin-select offset-lg-2 offset-md-2 offset-sm-2 offset-2 col-lg-2 col-md-2 col-sm-2 col-2">
+                            <optgroup style="background-color: white" label="@lang('common.users')">
+                                @foreach ($all_users as $user)
+                                    <option class="admin-option">{{ $user->login }}</option>
+                                @endforeach
+                            </optgroup>
+                        </select>
+                    </div>
                     <input type="button" class="admin-button col-lg-1 col-md-1 col-sm-1 col-1">
                 </div>
             </div>

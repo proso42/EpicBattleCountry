a72f760d0fff268de32cf2134e904a9d36cbb58e
Philippe <rosophilippe@gmail.com>
2019-11-29 12:47:08 +0100

    colonize [dev]
    test new algo for better cities name

---
+++ older_site/js/test.js
@@ -0,0 +1,44 @@
+let rdm_name = "";
+let con = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'z'];
+let voy = ['a', 'e', 'i', 'o', 'u', 'y'];
+let max = get_rdm_int(4);
+if (max == 0)
+    max = 1
+while (max > 0)
+{
+    let rdm = get_rdm_int(3);
+    if (rdm == 0)
+    {
+        // -- Tech 1 -- Con Voy Con
+        let lettre = con[get_rdm_int(20)];
+        while (lettre == rdm_name[rdm_name.length - 1])
+            lettre = con[get_rdm_int(20)];
+        rdm_name = rdm_name.concat(lettre, voy[get_rdm_int(6)], con[get_rdm_int(20)]);
+    }
+    else if (rdm == 1)
+    {
+        // -- Tech 2 -- Voy Con Con
+        let lettre = con[get_rdm_int(20)];
+        while (lettre == rdm_name[rdm_name.length - 1])
+            lettre = con[get_rdm_int(20)];
+        rdm_name = rdm_name.concat(lettre, voy[get_rdm_int(6)], con[get_rdm_int(20)]);
+    }
+    else
+    {
+        // -- Tech 3 -- Con Con Voy
+        let lettre = con[get_rdm_int(20)];
+        while (lettre == rdm_name[rdm_name.length - 1])
+            lettre = con[get_rdm_int(20)];
+        rdm_name = rdm_name.concat(lettre, voy[get_rdm_int(6)], con[get_rdm_int(20)]);
+    }
+    max--;
+}
+rdm_name = strUcFirst(rdm_name);
+console.log(rdm_name);
+
+function get_rdm_int(i)
+{
+    return Math.floor(Math.random() * Math.floor(i));
+}
+
+function strUcFirst(a){return (a+'').charAt(0).toUpperCase()+a.substr(1);}

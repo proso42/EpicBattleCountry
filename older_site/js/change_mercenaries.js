253cb991cde1bd960108f212832c6d54c63bede9
Philippe <rosophilippe@gmail.com>
2019-12-11 18:26:22 +0100

    mercenaries [WIP]
    - switch mercenaries now look at cool_down_mercenaries;

--- older_site/js/change_mercenaries.js
+++ older_site/js/change_mercenaries.js
@@ -22,28 +22,48 @@ mysqlClient.query(request_get_all_cities, function (err, results) {
         return (err);
     else
     {
-        mysqlClient.query("SELECT COUNT(id) AS 'max' FROM mercenaries", function (err, ret) {
-            let tab_p = [];
-            let max = ret[0]['max'];
-            for (let i = 0; i < results.length; i++)
-                tab_p.push(change_mercenraies(results[i], max));
-            Promise.all(tab_p)
-            .then(() => {
-                console.log('All availables mercenaries changed !');
-                return 0;
-            })
-            .catch((err) => {
-                console.log(err);
-                return 1;
+        mysqlClient.query(("SELECT city_id, tavern_slot FROM mercenaries_cool_down"), function (err, ret){
+            let cool_down = ret;
+            mysqlClient.query("SELECT COUNT(id) AS 'max' FROM mercenaries", function (err, ret) {
+                let tab_p = [];
+                let max = ret[0]['max'];
+                for (let i = 0; i < results.length; i++)
+                    tab_p.push(change_mercenraies(results[i], max, cool_down));
+                Promise.all(tab_p)
+                .then(() => {
+                    console.log('All availables mercenaries changed !');
+                    return 0;
+                })
+                .catch((err) => {
+                    console.log(err);
+                    return 1;
+                });
             });
         });
     }
 });
 
-function change_mercenraies(city, max)
+function change_mercenraies(city, max, cool_down)
 {
     return new Promise((resolve, reject) => {
-        if (city['tavern_slot1'] == -1)
+        c1 = 0;
+        c2 = 0;
+        c3 = 0;
+        for(let i = 0; i < cool_down.length; i++)
+        {
+            if (cool_down[i]['city_id'] != city['id'])
+                continue;
+            else
+            {
+                if (cool_down[i]['tavern_slot'] == 1)
+                    c1 = 1;
+                else if (cool_down[i]['tavern_slot'] == 2)
+                    c2 = 1;
+                else
+                    c3 = 1;
+            }
+        }
+        if (city['tavern_slot1'] == -1 || c1 == 1)
         {
             console.log(`For city ${city['id']} nothing ...`)
             resolve();
@@ -52,7 +72,7 @@ function change_mercenraies(city, max)
         {
             let new_mercenary_1 = Math.floor(Math.random() * Math.floor(max)) + 1;
             let new_quantity_1 = Math.trunc((getRandomInt(5000)+500)/100)*100;
-            if (city['tavern_slot2'] == -1)
+            if (city['tavern_slot2'] == -1 || c2 == 1)
             {
                 let req = `UPDATE cities SET tavern_slot1 = ${new_mercenary_1}, tavern_slot1_qt = ${new_quantity_1} WHERE id = ${city['id']}`;
                 console.log(req);
@@ -65,7 +85,7 @@ function change_mercenraies(city, max)
             {
                 let new_mercenary_2 = Math.floor(Math.random() * Math.floor(max)) + 1;
                 let new_quantity_2 = Math.trunc((getRandomInt(5000)+500)/100)*100;
-                if (city['tavern_slot3'] == -1)
+                if (city['tavern_slot3'] == -1 || c3 == 1)
                 {
                     mysqlClient.query(`UPDATE cities SET tavern_slot1 = ${new_mercenary_1}, tavern_slot2 = ${new_mercenary_2}, tavern_slot1_qt = ${new_quantity_1}, tavern_slot2_qt = ${new_quantity_2} WHERE id = ${city['id']}`, function (err, ret){
                         console.log(`For city ${city['id']} tavern_slot1 and tavern_slot2 UPDATED !`);

25e7416b56a9a13a2c92773f4d585eeb822a78c6
Philippe <rosophilippe@gmail.com>
2019-12-15 17:13:41 +0100

    mercenaries [fix]
    when all bullshit will over ?????

--- site/public/js/taverne.js
+++ site/public/js/taverne.js
@@ -49,7 +49,7 @@ function confirm_upgrade()
         {
             if (xhr.responseText == "Success")
             {
-                document.getElementById("slot" + g_slot + "_qt").style.textContent = 5000;
+                document.getElementById("slot" + g_slot + "_qt").textContent = 5000;
                 document.getElementById("success_mercenary_upgraded").style.display = "";
                 g_slot = 0;
                 setTimeout(() =>{

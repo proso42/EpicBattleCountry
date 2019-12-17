0ea44280763f88f9d451c0511712b12740de38c9
Philippe <rosophilippe@gmail.com>
2019-12-17 15:55:33 +0100

    mercenaries [debug]
    - debuging

--- site/app/Http/Controllers/MercenariesController.php
+++ site/app/Http/Controllers/MercenariesController.php
@@ -218,8 +218,7 @@
             ->where('id', '=', $city_id)
             ->update(['diamond' => $tavern_slot_DB->diamond - 1, $slot_db => $new_mercenary, $slot_qt_db => $new_quantity]);
             $new_mercenary = DB::table('mercenaries')->select('name')->where('id', '=', $new_mercenary)->first();
-            //return (["Success", trans('mercenaries.' . $new_mercenary), $new_quantity]);
-            return ("Success");
+            return (["Success", "ok", $new_quantity]);
         }
     }
 ?>
